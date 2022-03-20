fb09f93f40da29d8f03345f9fc536c81c2eb8822
john <john@lindahl.tech>
2022-03-20 20:32:20 +0100

    Let's mess with UA, only IP hiding left

--- src/main.mjs
+++ src/main.mjs
@@ -63,7 +63,7 @@ export async function main(config, debug){
       Origin: domain,
       Referer: `${domain}/${prefix}/${index}/${region}`,
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+      'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 ${Date.now()}`
     },
     cookieJar,
   })
