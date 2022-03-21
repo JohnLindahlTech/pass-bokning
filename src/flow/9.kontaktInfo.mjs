aa248d45b7bf3083b969740839f2c1577b925ef2
john <john@lindahl.tech>
2022-03-21 13:39:29 +0100

    email and phone is optional, but need atleast one

--- src/flow/9.kontaktInfo.mjs
+++ src/flow/9.kontaktInfo.mjs
@@ -7,26 +7,26 @@ export async function contactInfo(post, contact, debug){
   const {email, phone} = contact;
 
   const res2 = await post({
-    EmailAddress: email,
-    ConfirmEmailAddress: email,
-    PhoneNumber: phone,
-    ConfirmPhoneNumber: phone,
-    'SelectedContacts[0].IsSelected': true,
+    EmailAddress: email ?? '',
+    ConfirmEmailAddress: email ?? '',
+    PhoneNumber: phone ?? '',
+    ConfirmPhoneNumber: phone ?? '',
+    'SelectedContacts[0].IsSelected': !!email,
     'SelectedContacts[0].MessageTypeId': 2,
     'SelectedContacts[0].MessageKindId': 1,
     'SelectedContacts[0].TextName': 'MESSAGETYPE_EMAIL',
 
-    'SelectedContacts[1].IsSelected': true,
+    'SelectedContacts[1].IsSelected': !!phone,
     'SelectedContacts[1].MessageTypeId': 1,
     'SelectedContacts[1].MessageKindId': 1,
     'SelectedContacts[1].TextName': 'MESSAGETYPE_SMS',
 
-    'SelectedContacts[2].IsSelected': true,
+    'SelectedContacts[2].IsSelected': !!email,
     'SelectedContacts[2].MessageTypeId': 2,
     'SelectedContacts[2].MessageKindId': 2,
     'SelectedContacts[2].TextName': 'MESSAGETYPE_EMAIL',
 
-    'SelectedContacts[3].IsSelected': true,
+    'SelectedContacts[3].IsSelected': !!phone,
     'SelectedContacts[3].MessageTypeId': 1,
     'SelectedContacts[3].MessageKindId': 2,
     'SelectedContacts[3].TextName': 'MESSAGETYPE_SMS',
